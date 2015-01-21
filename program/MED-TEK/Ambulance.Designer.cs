4af5cb687f355213046915469a9fa5d10ba11aae
juleskreutzer <juleskreutzer@me.com>
2015-01-21 13:52:24 +0100

    Finished Beheer area

--- program/MED-TEK/Ambulance.Designer.cs
+++ program/MED-TEK/Ambulance.Designer.cs
@@ -209,7 +209,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Highlight;
-            this.ClientSize = new System.Drawing.Size(1684, 927);
+            this.ClientSize = new System.Drawing.Size(1784, 961);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
